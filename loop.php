d1cf5a02a1763419984a3da77a82644afd6841c5
Menna Hassan <mennahasan109@gmail.com>
2025-11-15 18:15:36 +0200

    Loop

--- loop.php
+++ loop.php
@@ -7,5 +7,45 @@
 </head>
 <body>
     
+
+<?php
+
+for($i = 0; $i < 10; $i++)
+{
+    echo $i. "<br>";
+}
+
+
+$num = 0;
+while($num < 10)
+{
+    echo $num. "<br>";
+    $num++ ;
+}
+
+
+$test = 10;
+do{
+    echo $test. "<br>";
+    $test++ ;
+}
+while($test<10);
+
+
+
+$family = ["Mom","Dad","sister","brother"];
+foreach($family as $p){
+    echo $p. "<br>";
+}
+
+
+
+$family2 = array("Mom"=>"Mona", "Dad"=>"Hassan", "sister"=>"Mai", "brother"=>"Ibrahim,Mohammed");
+foreach($family2 as $p => $name){
+    echo "My ". $p . " is ". $name .  "<br>";
+}
+
+
+?>
 </body>
 </html>
